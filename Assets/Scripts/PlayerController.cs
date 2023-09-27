360a8370fafe1b78583950a0155cfe9269d70f6a
Alan Sartorio <alan42ga@hotmail.com>
2023-09-27 16:15:01 -0300

    Add support for non VR

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private InputActionReference look;
+    [SerializeField] private InputActionReference forward;
+    [SerializeField] private InputActionReference backwards;
+    [SerializeField] private InputActionReference left;
+    [SerializeField] private InputActionReference right;
+
+    private float horizontalAngle;
+    private float verticalAngle;
+
+    [SerializeField] private float sensitivity = 0.5f;
+    [SerializeField] private float speed = 2f;
+
+    private Transform child;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        look.action.performed += Look;
+        var euler = transform.rotation.eulerAngles;
+        verticalAngle = 0;
+        horizontalAngle = euler.y;
+        child = transform.GetChild(0);
+    }
+
+    private void Look(InputAction.CallbackContext obj)
+    {
+        var delta = obj.ReadValue<Vector2>() * sensitivity;
+
+        verticalAngle -= delta.y;
+        verticalAngle = Mathf.Clamp(verticalAngle, -90, 90);
+        horizontalAngle += delta.x;
+        transform.localRotation = Quaternion.Euler(0, horizontalAngle, 0);
+        child.localRotation = Quaternion.Euler(verticalAngle, 0, 0);
+    }
+
+    void Update()
+    {
+        Transform objTransform = transform;
+        
+        if ((int)forward.action.ReadValue<float>() == 1)
+        {
+            objTransform.position += objTransform.forward * (speed * Time.deltaTime);
+        }
+
+        if ((int)backwards.action.ReadValue<float>() == 1)
+        {
+            objTransform.position -= objTransform.forward * (speed * Time.deltaTime);
+        }
+
+        if ((int)right.action.ReadValue<float>() == 1)
+        {
+            objTransform.position += objTransform.right * (speed * Time.deltaTime);
+        }
+
+        if ((int)left.action.ReadValue<float>() == 1)
+        {
+            objTransform.position -= objTransform.right * (speed * Time.deltaTime);
+        }
+    }
+}
