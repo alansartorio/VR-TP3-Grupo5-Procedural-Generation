4257a6d18528c2115c9e3f78e9972ec77dce5fac
Alan Sartorio <alan42ga@hotmail.com>
2023-09-28 15:43:36 -0300

    Remove clicking behaviour from PlayState

--- Assets/Scripts/States/PlayState.cs
+++ Assets/Scripts/States/PlayState.cs
@@ -8,7 +8,7 @@ public class PlayState : MonoBehaviour
 {
     [SerializeField] private EnemySpawner enemySpawner;
     [SerializeField] private EnemyWatcher enemyWatcher;
-    [SerializeField] private InputActionReference pointerPosition, attack;
+    // [SerializeField] private InputActionReference pointerPosition, attack;
 
     private void OnEnable()
     {
@@ -16,13 +16,13 @@ public class PlayState : MonoBehaviour
         enemySpawner.enabled = true;
 
         // pointerPosition.action.performed += PointerMoved;
-        attack.action.performed += AttackPerformed;
+        // attack.action.performed += AttackPerformed;
     }
 
     private void OnDisable()
     {
         // pointerPosition.action.performed -= PointerMoved;
-        attack.action.performed -= AttackPerformed;
+        // attack.action.performed -= AttackPerformed;
 
         enemySpawner.enabled = false;
         enemyWatcher.enabled = false;
@@ -34,19 +34,19 @@ public class PlayState : MonoBehaviour
         }
     }
 
-    private void AttackPerformed(InputAction.CallbackContext ctx)
-    {
-        var pos = pointerPosition.action.ReadValue<Vector2>();
-        var ray = Camera.main.ScreenPointToRay(pos);
-
-        if (!Physics.Raycast(ray, out var hitInfo))
-            return;
-
-        var hitTransform = hitInfo.transform;
-        if (hitTransform == null || !hitTransform.CompareTag("Enemy"))
-            return;
-
-        var enemy = hitTransform.GetComponent<EnemyBehaviour>();
-        enemy.Damage(2);
-    }
+    // private void AttackPerformed(InputAction.CallbackContext ctx)
+    // {
+    //     var pos = pointerPosition.action.ReadValue<Vector2>();
+    //     var ray = Camera.main.ScreenPointToRay(pos);
+    //
+    //     if (!Physics.Raycast(ray, out var hitInfo))
+    //         return;
+    //
+    //     var hitTransform = hitInfo.transform;
+    //     if (hitTransform == null || !hitTransform.CompareTag("Enemy"))
+    //         return;
+    //
+    //     var enemy = hitTransform.GetComponent<EnemyBehaviour>();
+    //     enemy.Damage(2);
+    // }
 }
